eb397de83d71aef792fdda40f8422172fdb1d738
Andrea Huang <huangandrea@hotmail.it>
2025-05-05 15:16:49 +0200

    fix typo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from "./store/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider sstore={store}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
